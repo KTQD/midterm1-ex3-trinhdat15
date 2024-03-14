1902d6b29e1f3104dd4cfe189339dcfa0c6bc19d
trinhdat15 <153842433+trinhdat15@users.noreply.github.com>
2024-03-14 08:31:25 +0700

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-
-
-        Bài làm viết tại đây
-
+        String inputFile = "input.txt";
+        String outputFile = "output.txt";
+        try {
+            BufferedReader reader = new BufferedReader (new FileReader(inputFile));
+            StringBuilder content = new StringBuilder();
+            String line;
+            while((line = reader.readLine())!= null) {
+                content.append(line).append("\n");
+            }
+            reader.close();
+            String replacedContent = content.toString().replaceAll("NHa Trang", "Vũng Tàu");
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+            writer.write(replacedContent);
+            writer.close();
+            System.out.println("File output.txt ghi thanh cong");
+        } catch (IOException e) {
+            System.out.println("Da xay ra loi khi doc file");
+        }
     }
 }
